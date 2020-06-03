8f1eed0b1beae720e8b7972212b0b49491970c0b
fabiovenicius <fabiovenicius@yahoo.com.br>
2020-06-03 13:47:33 -0300

    VueJS on desktop

---
+++ most-used-words/src/components/Home.vue
@@ -0,0 +1,90 @@
+<template>
+    <v-container fluid>
+        <v-form>
+            <v-file-input
+            label = "Selecione as legendas"
+            prepend-icon="mdi-message-text"
+            outlined
+            append-outer-icon="mdi-send"
+            multiple 
+            chips 
+            v-model="files"
+            @click:append-outer="processSubtitles"/>
+
+        </v-form>
+        <div class="pills">
+            <Pill v-for="word in groupedWords" :key="word.name"
+                :name="word.name" :amount="word.amount"/>
+        </div>
+    </v-container>
+    
+
+
+</template>
+
+<script>
+import Pill from './Pill'
+
+export default {
+    components: {Pill} ,
+
+    data: function(){
+        return{
+            files:[],
+            groupedWords:[
+                {name:'i',amount:1234},
+                {name:'you', amount:645},
+                {name:'he',amount:768},
+                {name:'i',amount:1234},
+                {name:'you', amount:645},
+                {name:'he',amount:768},
+                {name:'i',amount:1234},
+                {name:'you', amount:645},
+                {name:'he',amount:768},
+                {name:'i',amount:1234},
+                {name:'you', amount:645},
+                {name:'he',amount:768},
+                {name:'i',amount:1234},
+                {name:'you', amount:645},
+                {name:'he',amount:768},
+                {name:'i',amount:1234},
+                {name:'you', amount:645},
+                {name:'he',amount:768},
+                {name:'i',amount:1234},
+                {name:'you', amount:645},
+                {name:'he',amount:768},
+                {name:'i',amount:1234},
+                {name:'you', amount:645},
+                {name:'he',amount:768},
+                {name:'i',amount:1234},
+                {name:'you', amount:645},
+                {name:'he',amount:768},
+                {name:'i',amount:1234},
+                {name:'you', amount:645},
+                {name:'he',amount:768},
+                {name:'i',amount:1234},
+                {name:'you', amount:645},
+                {name:'he',amount:768},
+                {name:'i',amount:1234},
+                {name:'you', amount:645},
+                {name:'he',amount:768}
+            ]
+        }
+    },
+    methods:{
+        processSubtitles(){
+            console.log(this.files)
+        }
+    }
+
+}
+</script>
+
+<style>
+    .pills{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+
+</style>
---
+++ most-used-words/src/components/Pill.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="pill">
+    <span class="name"> {{ name}}  </span>
+    <span class="amount"> {{ amount}}  </span>
+
+  </div>
+</template>
+
+<script>
+export default {
+    props:['name','amount']
+}
+</script>
+
+<style scoped>
+    .pill{
+        margin: 10px;
+        height: 55px;
+        border-radius: 30px;
+
+        background-color: rgb(233, 29, 29);
+        color: #ffffff;
+        font-size: 1.3 rem;
+        font-weight: bold;
+
+        display:flex;
+        align-items: center;
+
+    }
+
+    .pill .name{
+      margin-left: 15px;      
+    }
+
+    .pill .amount{
+        margin: 10px;
+        padding: 5px;
+        height: 35px;
+        width: 35px;
+        border-radius: 18px;
+
+        background-color: #fff;
+        color: #dc143c;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        font-size: 12px;
+        
+
+    }
+
+
+</style>
